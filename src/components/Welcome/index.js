830cb56d2b132599679a32d813696b68e4db43fa
Tim Mendenhall <timmendenhall@gmail.com>
2021-07-10 20:42:38 -0400

    Adding the welcome component / card

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -3,15 +3,22 @@ import './Welcome.css';
 
 export const Welcome = () => {
     return (
-        <div className="welcome__container">
-            <div className="welcome__card">
+        <>
+            <div className="welcome__background"></div>
+            <div className="welcome__container">
+                <div className="welcome__card">
                 <span className="welcome__card-greeting">
                     Hello, I'm
                 </span>
                 <span className="welcome__card-greeting">
                     Tim Mendenhall
                 </span>
+                <span className="welcome__card-greeting-small">
+                    Full-stack Web Developer
+                </span>
+                </div>
             </div>
-        </div>
+        </>
+
     );
 }
