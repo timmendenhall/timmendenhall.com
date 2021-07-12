07450fc4b8003351582de88222ff39c7112b1550
Tim Mendenhall <timmendenhall@gmail.com>
2021-07-12 18:50:15 -0400

    Social Bar updates for GH and LI links

--- src/components/SocialBar/index.js
+++ src/components/SocialBar/index.js
@@ -4,8 +4,10 @@ import './SocialBar.css';
 export const SocialBar = () => {
     return (
         <div className="social-bar__container">
-            <button type="button" className="social-bar__button">GitHub</button>
-            <button type="button" className="social-bar__button">LinkedIn</button>
+            <a href="https://github.com/timmendenhall" className="social-bar__button" target="new"><i
+                className="fab fa-github"></i> GitHub</a>
+            <a href="https://www.linkedin.com/in/timmendenhall/" className="social-bar__button" target="new"><i
+                className="fab fa-linkedin-in"></i> LinkedIn</a>
         </div>
     );
 }
