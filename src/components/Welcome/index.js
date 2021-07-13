5f49e1fef2b0b1cbd63a5bc66752b603fb0e22a2
Tim Mendenhall <timmendenhall@gmail.com>
2021-07-12 21:35:33 -0400

    Small updates to font, sizing added profile pic

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Welcome.css';
 import {SocialBar} from "../SocialBar";
+import profileImage from './profile-pic.jpg';
 
 export const Welcome = () => {
     return (
         <>
             <div className="welcome__background"/>
             <div className="welcome__container">
+                <img src={profileImage} className="welcome__profile-image"/>
                 <div className="welcome__card">
                 <span className="welcome__card-greeting">
                     Hello, I'm
